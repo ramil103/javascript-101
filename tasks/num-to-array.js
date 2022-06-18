83f34ebbc3b7b07f176644814893e87689c31a1a
Radik Fattakhov <dev@radik.dev>
2022-06-18 08:37:22 +0200

    num to array

---
+++ tasks/num-to-array.js
@@ -0,0 +1,15 @@
+// Написать функцию, которая на вход принимает число и превращает его в массив цифр
+// 0 -> [0]
+// 6782 -> [6, 7, 8, 2]
+// -1 -> [-1]
+// -239874 -> [-2, 3, 9, 8, 7, 4]
+
+
+function numToArray(num) {
+
+}
+
+console.log(numToArray(0), [0]);
+console.log(numToArray(6782), [6, 7, 8, 2]);
+console.log(numToArray(-1), [-1]);
+console.log(numToArray(-239874), [-2, 3, 9, 8, 7, 4]);
