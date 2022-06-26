b5a5b81e7e6324add73962bce765989f2930f309
Radik Fattakhov <dev@radik.dev>
2022-06-26 09:49:49 +0200

    API

---
+++ tasks/api/google-books.js
@@ -0,0 +1,12 @@
+import fetch from "node-fetch";
+
+// Создать функцию для получения списка книг с Google Books API
+// Документация API https://developers.google.com/books/docs/v1/using#WorkingVolumes
+// Функция должна давать возможность фильтровать книги:
+// - по автору (author)
+// - по названию (title)
+// - по издателю (publisher)
+function getBooks() {
+
+}
+
